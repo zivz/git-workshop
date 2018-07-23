a05dc4d0b00db8f0ed807e8329ab85ad98c620e4
Ziv Zalzstein <zivzaltz@gmail.com>
2018-07-23 12:38:51 +0300

    Added div method

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,5 +7,14 @@ public class Calculator {
 	public int mul(int a, int b){
 		return a*b;
 	}
+
+ 	public double div(int a, int b){
+	if (b != 0) {
+		return a/b; 
+	}
+	else {
+	throw new ArithmeticException("Division By Zero");
+	}
+	}
 } 
 
